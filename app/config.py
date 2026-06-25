6e96b4b4c8c56436a0cc7dd5a82778736359d352
Jongeum-k <k.jongeum@gmail.com>
2026-06-25 15:48:51 -0400

    Add config

---
+++ app/config.py
@@ -0,0 +1,21 @@
+import os
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    env: str = "dev"
+    debug: bool = False
+    log_level: str = "info"
+    database_url: str
+
+    class Config:
+        env_file = {
+            "prod": ".env.prod",
+            "demo": ".env.demo"
+        }.get(os.getenv("ENV", "dev"), ".env")
+        extra = "ignore"
+
+    @property
+    def db_url(self):
+        return self.database_url
+
+settings = Settings()
